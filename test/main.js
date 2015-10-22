c6267061452f476ec6cf9f3c1fe94468aaf03b38
Retep <retep@MacBook-Pro-de-Retep.local>
2015-10-22 22:37:07 +0200

    First commit Features : 	- Ajax object 	- Shortcut _log, _id, _qs, _parse.

---
+++ test/main.js
@@ -0,0 +1,47 @@
+window.onload = function () {
+	_id('ajax'    ).addEventListener('submit', sendAjax);
+	_id('ajaxForm').addEventListener('submit', sendAjaxFormData);
+};
+
+function sendAjax (e) {
+	e.preventDefault();
+
+	var call = new Ajax({
+		url   : 'script.php',
+		params: {
+			name: _qs('[name="name"]').value
+		}
+	});
+
+	call.success(function (data) {
+		_log(data);
+	});
+
+	call.error(function (data) {
+		_log(data);
+	});
+
+	return false;
+}
+
+function sendAjaxFormData (e) {
+	e.preventDefault();
+
+	var form = new FormData(this);
+
+	var call = new Ajax({
+		url     : 'script.php',
+		params  : form        ,
+		formdata: true
+	});
+
+	call.success(function (data) {
+		_log(data);
+	});
+
+	call.error(function (data) {
+		_log(data);
+	});
+
+	return false;
+}
---
+++ test/script.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+	Basic PHP script to test jTep
+	Return string
+		'Hello {{POST->name}}'
+*/
+
+echo ('Hello : ' . $_POST['name'] );
